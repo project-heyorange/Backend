e8dabff19a18e6176469d35dfa773b0aecf17fec
ruandias <ruan_ndias@hotmail.com>
2022-04-03 22:07:48 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/heyorange/heyorange/HeyorangeApplication.java
@@ -0,0 +1,13 @@
+package com.heyorange.heyorange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeyorangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeyorangeApplication.class, args);
+	}
+
+}
