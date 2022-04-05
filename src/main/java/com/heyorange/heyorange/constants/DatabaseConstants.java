d0b1412baf49110714c60091512eda5db76a592c
ruandias <ruan_ndias@hotmail.com>
2022-04-05 00:22:32 -0300

    adicionado database constant

---
+++ src/main/java/com/heyorange/heyorange/constants/DatabaseConstants.java
@@ -0,0 +1,9 @@
+package com.heyorange.heyorange.constants;
+
+import lombok.NoArgsConstructor;
+import lombok.AccessLevel;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DatabaseConstants {
+	public static final String HEY_ORANGE = "heyorange";
+}
