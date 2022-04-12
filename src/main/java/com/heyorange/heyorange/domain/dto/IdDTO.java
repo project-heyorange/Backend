ce7759bf9f49f19fdb4dff1f98393e774927337c
ruandias <ruan_ndias@hotmail.com>
2022-04-12 15:51:46 -0300

    Adicionado a camada DTO

---
+++ src/main/java/com/heyorange/heyorange/domain/dto/IdDTO.java
@@ -0,0 +1,16 @@
+package com.heyorange.heyorange.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class IdDTO {
+
+	private Long id;
+
+}
