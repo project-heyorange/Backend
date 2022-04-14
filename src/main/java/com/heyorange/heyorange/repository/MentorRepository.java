c7a400c5b3e936f1791c65a842f6a695b7a632dd
ruandias <ruan_ndias@hotmail.com>
2022-04-13 23:52:21 -0300

    Adicionado query com join nas classes Mentor, Mentorado e Pessoa

--- src/main/java/com/heyorange/heyorange/repository/MentorRepository.java
+++ src/main/java/com/heyorange/heyorange/repository/MentorRepository.java
@@ -1,11 +1,18 @@
 package com.heyorange.heyorange.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.heyorange.heyorange.domain.entity.Mentor;
 
 @Repository
 public interface MentorRepository extends JpaRepository<Mentor, Long> {
+
+	@Query("from Mentor m join m.pessoa u join fetch m.pessoa where u.nome = :nome")
+	Optional<Mentor> findByNome(@Param("nome") String nome);
 
 }
--- src/main/java/com/heyorange/heyorange/repository/PessoaRepository.java
+++ src/main/java/com/heyorange/heyorange/repository/PessoaRepository.java
@@ -1,9 +1,16 @@
 package com.heyorange.heyorange.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.heyorange.heyorange.domain.entity.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
+	@Query("from Pessoa p where p.nome =: nome")
+	Optional<Pessoa> findByNome(@Param("nome") String nome);
 
 }
