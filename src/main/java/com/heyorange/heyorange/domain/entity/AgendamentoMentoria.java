e362a5ed07cad355edd8cca34b39659cd1442483
ruandias <ruan_ndias@hotmail.com>
2022-04-13 23:47:00 -0300

    ajustando ManyToOne para CascadeType

--- src/main/java/com/heyorange/heyorange/domain/entity/AgendamentoMentoria.java
+++ src/main/java/com/heyorange/heyorange/domain/entity/AgendamentoMentoria.java
@@ -1,32 +1,34 @@
 package com.heyorange.heyorange.domain.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+
+import com.heyorange.heyorange.constants.DatabaseConstants;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import com.heyorange.heyorange.constants.DatabaseConstants;
 
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
 @Entity
 @Table(schema = DatabaseConstants.HEY_ORANGE, name = "AGENDAMENTO_MENTORIA")
 public class AgendamentoMentoria extends AbstractEntity<Long> {
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(cascade = CascadeType.PERSIST)
 	@JoinColumn(name = "ID_ENVIO_EMAIL", referencedColumnName = "ID")
 	private EnvioEmail envioEmail;
 
-	@ManyToOne(fetch = FetchType.LAZY)
+	@ManyToOne(cascade = CascadeType.PERSIST)
 	@JoinColumn(name = "ID_CONFIRMACAO_MENTORIA", referencedColumnName = "ID")
 	private ConfirmacaoMentoria confirmacaoMentoria;
 
