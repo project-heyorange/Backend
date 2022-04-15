d8cd59451530cfe7b4c93c5acddd96d3de564703
Alexandre <AlexDLC2020@outlook.com>
2022-04-15 16:22:58 -0300

    Adicionando cross origins para permitir interação com o Front

--- src/main/java/com/heyorange/heyorange/controller/AgendamentoMentoriaController.java
+++ src/main/java/com/heyorange/heyorange/controller/AgendamentoMentoriaController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,6 +25,7 @@ import com.heyorange.heyorange.service.MentoriaService;
 
 @RestController
 @RequestMapping("/agendamento")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AgendamentoMentoriaController extends BaseController {
 
 	@Autowired
--- src/main/java/com/heyorange/heyorange/controller/PessoaController.java
+++ src/main/java/com/heyorange/heyorange/controller/PessoaController.java
@@ -4,6 +4,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +21,7 @@ import com.heyorange.heyorange.service.PessoaService;
 
 @RestController
 @RequestMapping("/pessoas")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PessoaController extends BaseController {
 
 	@Autowired
